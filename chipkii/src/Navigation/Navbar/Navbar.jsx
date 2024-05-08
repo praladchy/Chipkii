686c828fa67078711829c913d4692b252a9ceebd
Pralad Chaudhary <praladchy028@gmail.com>
2024-05-08 12:12:24 +0545

    responsive nav bar

--- chipkii/src/Navigation/Navbar/Navbar.jsx
+++ chipkii/src/Navigation/Navbar/Navbar.jsx
@@ -9,29 +9,33 @@ import { FaShoppingCart } from "react-icons/fa";
 const Navbar = () => {
   const [Decoration, SetDecoration] = useState("Menu")
   return (
-    <div className='Navbar-Main-Container'>
-      <div className="Logo-Main-Container">
+    <div className='Navbar-Main-Container navbar navbar-expand-lg navbar-light bg-light'>
+      <div className="Logo-Main-Container navbar-brand">
         <h2><Lable Lable="Chipkii" /></h2>
       </div>
-      <div className="Navbar-Main-Menu-Container">
-        <Link onClick={() => SetDecoration("Home")} className={Decoration === "Home" ? "Active" : "Navbar-Main-Menu-Container"} to="/">Home</Link>
-        {/* <Link></Link> */}
-        <NavLink text-decoration="none" className={Decoration === "Menu" ? "Active" : "Navbar-Main-Menu-Container"} onClick={() => SetDecoration("Menu")} to="/Menu">Menu</NavLink>
-        <NavLink text-decoration="none" className={Decoration === "Mobileapp" ? "Active" : "Navbar-Main-Menu-Container"} onClick={() => SetDecoration("Mobileapp")} to="/Mobileapp">Mobileapp</NavLink>
-        <NavLink text-decoration="none" className={Decoration === "ContactUs" ? "Active" : "Navbar-Main-Menu-Container"} onClick={() => SetDecoration("ContactUs")} to="/ContactUs">ContactUs</NavLink>
-      </div>
-      <div className="Navbar-Main-Right-Container">
-        <CiSearch />
-       <Link to="/Cart"><FaShoppingCart /></Link> 
-        <div className="Navbar-Right-Side-Cart-Container">
-         
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <div className="Navbar-Main-Menu-Container navbar-nav mr-auto mt-2 mt-lg-0">
+          <Link onClick={() => SetDecoration("Home")} className={Decoration === "Home" ? "Active" : "Navbar-Main-Menu-Container nav-item"} to="/">Home</Link>
+          {/* <Link></Link> */}
+          <NavLink text-decoration="none" className={Decoration === "Menu" ? "Active" : "Navbar-Main-Menu-Container nav-item"} onClick={() => SetDecoration("Menu")} to="/Menu">Menu</NavLink>
+          <NavLink text-decoration="none" className={Decoration === "Mobileapp" ? "Active" : "Navbar-Main-Menu-Container nav-item"} onClick={() => SetDecoration("Mobileapp")} to="/Mobileapp">Mobileapp</NavLink>
+          <NavLink text-decoration="none" className={Decoration === "ContactUs" ? "Active" : "Navbar-Main-Menu-Container nav-item"} onClick={() => SetDecoration("ContactUs")} to="/ContactUs">ContactUs</NavLink>
         </div>
+        <div className="Navbar-Main-Right-Container">
+          <CiSearch />
+          <Link to="/Cart"><FaShoppingCart /></Link>
+          <div className="Navbar-Right-Side-Cart-Container">
 
-       <Link to="/Login"><button className='Navbar-Button-Container' > Login</button></Link> 
+          </div>
+
+          <Link to="/Login"><button className='Navbar-Button-Container' > Login</button></Link>
+        </div>
+        </div >
       </div>
-
-    </div>
-  )
+      )
 }
 
-export default Navbar
+      export default Navbar
